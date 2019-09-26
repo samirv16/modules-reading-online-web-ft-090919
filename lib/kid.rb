9c92900084b3f6dda28876130b923c6dd56b2019
edras vallecillo <samir.vallecillo@outlook.com>
2019-09-26 20:03:07 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -4,6 +4,9 @@ require_relative './class_methods_module.rb'
 class Kid
   extend MetaDancing
   include Dance
+  
+  extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
  
   attr_accessor :name
  
